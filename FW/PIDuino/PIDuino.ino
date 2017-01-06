cb2e14dee1f179ec471ee7e384845b74fa651f57
Luis <hfegetude@gmail.com>
2017-01-06 13:02:05 +0100

    Added temp bar

--- FW/PIDuino/PIDuino.ino
+++ FW/PIDuino/PIDuino.ino
@@ -3,7 +3,6 @@
 int temp2analog(int temp) {
   return temp * 2 + 103;
 }
-
 PID pid_controller(3, 7, 10, 0, 4, 100);
 int aim = 35;
 void setup() {
@@ -13,13 +12,19 @@ void setup() {
 
 void loop() {
   int output;
+  char data_b[4];
   output = pid_controller.readData();
   int power = pid_controller.calculateOutput();
   pid_controller.setOutput();
   Serial.println("st");
   Serial.println(output);
   Serial.println(power);
-  Serial.println(aim);
+  Serial.println(pid_controller.aim);
   Serial.println(millis());
+  if(Serial.available() > 0){
+    Serial.readBytes(data_b, 4);
+    String buf2st(data_b);
+    pid_controller.setAim(temp2analog(buf2st.toInt()/100)); 
+  }
   pid_controller.wait();
 }
--- FW/grapher.py
+++ FW/grapher.py
@@ -1,17 +1,22 @@
 import numpy
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.widgets import Slider
+from matplotlib.patches import Rectangle
 import serial
 
 def data_gen():
+    currentMean = 0
     while True:
         c = ser.readline();
         if c.find('s') != -1:
             temp = ( 5*float(ser.readline())/1024.0 -0.5)*100
             power =float(ser.readline())/255*10
-            aim = float(ser.readline())
+            aim = (5*float(ser.readline())/1024.0 - 0.5)*100
             ti = float(ser.readline())/1000
-            yield [temp, power, aim,  ti]
+            currentMean = (temp + currentMean)/2
+            
+            yield [temp, power, aim,  ti, currentMean]
 def init():
     ax.set_ylim(0, 50)
     ax.set_xlim(0, 10)
@@ -32,8 +37,8 @@ def run(data):
     adata.append(data[2])
     pdata.append(data[1])
     ydata.append(data[0])
+    TextBox.set_text( '{0:.3f}'.format( data[4] ) )
     xmin, xmax = ax.get_xlim()
-
     if data[3] >= xmax:
         ax.set_xlim(xdata[0], data[3])
         xdata.pop(0)
@@ -51,7 +56,15 @@ def run(data):
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 fig, ax = plt.subplots()
+TextBox = ax.text(0.5, 1.0, str(0), transform=ax.transAxes, ha="right", va="bottom", color="black", family="sans-serif", fontweight="light", fontsize=16)
 
+axamp = plt.axes([0.12, 0.02, 0.8, 0.03], axisbg='b')
+stemp = Slider(axamp, 'Temp', 20, 50, valinit=5)
+
+
+def send_temp(val):
+    ser.write( '{0:.0f}'.format(stemp.val*100) )
+stemp.on_changed(send_temp)
 line, = ax.plot([], [], lw=2)
 line2, = ax.plot([], [], lw=2 )
 line3, = ax.plot([], [], lw=2 )
@@ -60,4 +73,5 @@ xdata, ydata = [], []
 pdata, adata = [], []
 ani = animation.FuncAnimation(fig, run, data_gen, blit=False,
                               repeat=False, init_func=init)
+
 plt.show()
